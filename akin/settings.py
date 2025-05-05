a798b9b3b3358ee7c9436f615e2f1f5d5fcd77da
Tilman Porschuetz <mandymozart@viennastruggle.com>
2025-05-05 17:28:34 +0200

    add docs to apps

--- akin/settings.py
+++ akin/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'docs'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'djangoproject.urls'
+ROOT_URLCONF = 'akin.urls'
 
 TEMPLATES = [
     {
