fdf450ead4a75417eb9a39674bbf550a7f1f5586
Tilman Porschuetz <mandymozart@viennastruggle.com>
2025-05-05 17:45:55 +0200

    maybe

--- docs/views/home.py
+++ docs/views/home.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def HomeView(request):
     message = "Hello, World!"
-    return render(request, 'views/docs/home.html', {'message': message})
+    return render(request, 'templates/docs/home.html', {'message': message})
